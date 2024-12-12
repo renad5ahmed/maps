311c34996c543516d11220a2e7e452b75148ecc4
Renad Ahmed <renadahmed581@gmail.com>
2024-12-12 10:58:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const popupCard = document.getElementById('popup-card');
-    const popupInfo = document.getElementById('popup-info');
+function showParagraph(index) {
+    
+    const titles = document.querySelectorAll('.title');
+    titles.forEach(title => title.classList.remove('active'));
+    titles[index].classList.add('active');
 
-    window.display = function(area) {
-        const coords = area.getAttribute('coords').split(',');
-        const [x, y, r] = coords.map(Number);
-        const test=area.getAttribute('data-description').split(',');
-        const[z1,z2]=test.map(String);
-
-        popupInfo.innerHTML = `
-            <p> <strong> ${area.alt}</strong></p>
-            <p>${z1}
-            </p>
-            <p> ${z2}
-          
-            </p>
-        `;
-
-        popupCard.style.display = 'block';
-        popupCard.style.top = `${y }px`;
-        popupCard.style.left = `${x + r }px`;
-    }
-
-    window.hideCard = function() {
-        popupCard.style.display = 'none';
-    }
-
-    window.moreInfo = function() {
-        alert('More Info clicked!');
-    }
-
-    window.contactUs = function() {
-        alert('Contact Us clicked!');
-    }
-});
-
+    const paragraphs = document.querySelectorAll('.paragraph');
+    paragraphs.forEach(paragraph => paragraph.classList.remove('active'));
+    document.getElementById(`paragraph-${index}`).classList.add('active');
+  }
+    document.addEventListener('DOMContentLoaded', () => {
+    showParagraph(0);
+  });
+  
